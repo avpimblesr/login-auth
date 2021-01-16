2e5c7209c51a8133dfcdcc3e76c8124f063da5be
avpimblesr <allan.pimble@gmail.com>
2021-01-16 17:43:28 -0500

    Added state to the Login form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 import routes from './Config/routes'
+import { AuthProvider } from './Context'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        {routes.map((route) => (
-          <Route
-            key={route.path}
-            exact
-            path={route.path}
-            component={route.component}
-          />
-        ))}
-      </Switch>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Switch>
+          {routes.map((route) => (
+            <Route
+              key={route.path}
+              exact
+              path={route.path}
+              component={route.component}
+            />
+          ))}
+        </Switch>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './login.module.css'
 
 const Login = (props) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <div className={styles.container}>
-      <div className={styles.formContainer}>
+      <div className={{ width: 200 }}>
         <h1>Login Page</h1>
 
         <form>
           <div className={styles.loginForm}>
             <div className={styles.loginFormItem}>
               <label htmlFor='email'>Username</label>
-              <input type='text' id='email' />
+              <input
+                type='text'
+                id='email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
 
             <div className={styles.loginFormItem}>
               <label htmlFor='password'>Password</label>
-              <input type='password' id='password' />
+              <input
+                type='password'
+                id='password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
           </div>
           <button>login</button>
