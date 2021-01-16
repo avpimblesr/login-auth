2a3b08b7956dc5c4f583c64c2ef39a895cd7761c
avpimblesr <allan.pimble@gmail.com>
2021-01-16 11:02:57 -0500

    Created Login page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 import routes from './Config/routes'
+import Login from './Pages/Login/index'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        {routes.map((route) => (
-          <Route
-            key={route.path}
-            path={route.path}
-            component={route.component}
-          />
-        ))}
-      </Switch>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Switch>
+    //     {routes.map((route) => (
+    //       <Route
+    //         key={route.path}
+    //         path={route.path}
+    //         component={route.component}
+    //       />
+    //     ))}
+    //   </Switch>
+    // </BrowserRouter>
+    <>
+      <Login />
+    </>
   )
 }
 
